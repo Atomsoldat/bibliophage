5e8fde77fbfca511769b2564bcb70a575fdbcd40
Leon Welchert <lichturm@posteo.de>
2025-12-16 03:24:25 +0100

    override v-bind rule for vue to prefer long form at all times

--- web-ui/eslint.config.mjs
+++ web-ui/eslint.config.mjs
@@ -1,4 +1,13 @@
 // eslint.config.mjs
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+// https://github.com/antfu/eslint-config?tab=readme-ov-file#rules-overrides
+// https://eslint.vuejs.org/user-guide/
+// https://eslint.vuejs.org/rules/
+
+export default antfu({
+  rules: {
+    // override default which would abbreviate v-bind:xyz to :xyz
+    'vue/v-bind-style': ['error', 'longform'],
+  },
+})
