91582823a5f58b971a55c7ee6f8f468f70e15854
Leon Welchert <lichturm@posteo.de>
2025-12-31 12:37:21 +0100

    refactor: turn application console into a regular view component  that respects the sidebar

--- web-ui/src/App.vue
+++ web-ui/src/App.vue
@@ -2,14 +2,22 @@
 import GlobalConsole from './components/GlobalConsole.vue'
 import GlobalEditorWindows from './components/GlobalEditorWindows.vue'
 import Sidebar from './components/Sidebar.vue'
+import { useAppConsole } from './composables/useAppConsole'
+
+const { isVisible } = useAppConsole()
 </script>
 
 <template>
-  <div class="min-h-screen">
+  <div class="min-h-screen flex flex-col">
     <Sidebar />
-    <main class="ml-16 p-8">
+    <!-- Main content area that adjusts height based on console visibility -->
+    <main
+      class="ml-16 p-8 overflow-auto transition-all duration-300"
+      :style="{ height: isVisible ? '50vh' : '100vh' }"
+    >
       <router-view />
     </main>
+    <!-- Console now part of layout flow, not a modal -->
     <GlobalConsole />
     <GlobalEditorWindows />
   </div>
